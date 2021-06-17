1764239a727c5cb4618bf19e60cc23afd8c54f89
Tanuka Das <tanuka.das12@gmail.com>
2021-06-17 17:06:04 -0400

    leetcode 230 - iterative

--- kthSmallest.js
+++ kthSmallest.js
@@ -25,3 +25,41 @@ var kthSmallest = function(root, k){
   return arr[k-1]
 
 }
+//-----------------------------------------------------------------------------
+
+var kthSmallest = function(root, k) {
+
+  let stack = []
+
+  let count = 0
+
+  if(root === null) return
+
+  while(true){
+
+    if(root !== null){
+
+      stack.push(root)
+
+      root = root.left
+
+    }else{
+
+      if(stack.length === 0)  break
+
+      root = stack.pop()
+
+      count++
+
+      if(count === k) return root.val
+
+      root = root.right
+
+    }
+
+  }
+
+};
+console.log(kthSmallest([3,1,4,null,2], 1));
+
+console.log(kthSmallest([5,3,6,2,4,null,null,1], 3));
