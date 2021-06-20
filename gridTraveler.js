6153478aa7b1ba496eebd714f1896b5ccf472d0e
Tanuka Das <tanuka.das12@gmail.com>
2021-06-19 23:06:28 -0400

    gridTraveler memoization

--- gridTraveler.js
+++ gridTraveler.js
@@ -1,6 +1,6 @@
 /*
   Approaches
-    a. recurssion
+    a. recursion
     b. memoization
     c. dynamic programming
 */
@@ -14,5 +14,26 @@ function gridTraveler(m, n){
 
   //recursive case
   return gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+}
+console.log(gridTraveler(2, 3))
+
+// memoization
+
+function gridTraveler(m, n){
+  let memo = {}
+
+  let key = m + ',' + n
+
+  if(key in memo) return memo[key]
+  //base case
+  if(m === 1 && n === 1)  return 1
+
+  if(m === 0 || n === 0)  return 0
+
+  //recursive case
+  memo[key] = gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+
+  return memo[key]
+
 }
 console.log(gridTraveler(2, 3))
