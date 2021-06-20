1f912262a2cd55e281398d556df1989842c87cb4
Tanuka Das <tanuka.das12@gmail.com>
2021-06-19 23:15:47 -0400

    gridTraveler dp

--- gridTraveler.js
+++ gridTraveler.js
@@ -4,6 +4,16 @@
     b. memoization
     c. dynamic programming
 */
+/*
+                              (2, 3)
+                      /                    \
+              (1, 3)                      (2, 2)
+            /        \             /           \
+        (0, 3)      (1, 2)       (1, 2)        (2, 1)
+        /   \        /   \       /     \       /     \
+  (0, 2) (1, 1) (0, 2) (1, 1) (0, 2) (1, 1)  (0, 2) (1, 1)
+
+*/
 
 function gridTraveler(m, n){
 
@@ -35,5 +45,36 @@ function gridTraveler(m, n){
 
   return memo[key]
 
+}
+console.log(gridTraveler(2, 3))
+
+/* dynamic programming
+    0 0 0 0
+    0 1 1 1
+    0 1 2 3
+    0 1 3 6
+*/  
+function gridTraveler(m, n){
+
+  let table = Array(m + 1).fill().map(() => Array(n + 1).fill(0))
+
+  table[1][1] = 1
+
+  // iterate over the row & col
+  for(let row = 0; row <= m; row++){
+
+    for(let col = 0; col <= n; col++){
+
+      let current = table[row][col]
+
+      // if it doesn't breach the boundary -> add below
+      if(col + 1 <= n)  table[row][col + 1] += current
+
+      // if it doesn't breach the boundary -> add to the right
+      if(row + 1 <= m)  table[row + 1][col] += current
+    }
+  }
+
+  return table[m][n]
 }
 console.log(gridTraveler(2, 3))
